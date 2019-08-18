1f1bbb20e9273111eca0e28d1a9df7332a43b212
handsome0hell <handsome0hell@gmail.com>
2019-08-19 00:01:42 +0800

    Finished surface rendering
    All basic 2D rendering (with low performance).

--- Main.java
+++ Main.java
@@ -5,11 +5,12 @@ import ThreeD.Engine.Renderer;
 public class Main{
   public static void main(String args[]) {
     Space main_engine = new Space();
-    main_engine.AddPoint(new Vector3D(0, 0, 0));
-    main_engine.AddPoint(new Vector3D(6, 0, 4));
+    main_engine.AddPoint(new Vector3D(0, 0, 2));
+    main_engine.AddPoint(new Vector3D(5, 0, 6));
     Renderer main_renderer = new Renderer(10);
     main_renderer.Fill(' ');
-    main_renderer.RenderLine(new Vector2D(0, 0), new Vector2D(6, 4));
+    main_renderer.RenderSurface(new Vector2D(0, 0), new Vector2D(6, 4), new Vector2D(5, 1));
+    main_renderer.RenderLine(new Vector2D(0, 2), new Vector2D(5, 6));
     main_renderer.Render(main_engine);
     main_renderer.Display();
   }
--- ThreeD/Engine/Renderer.java
+++ ThreeD/Engine/Renderer.java
@@ -5,9 +5,15 @@ import ThreeD.Linear.Vector3D;
 public class Renderer {
   private char screen_[][];
   private int size_;
+  private Vector2D offset_;
+  private void SetScreen(Vector2D position, char value) {
+    screen_[position.GetX() + offset_.GetX()]
+           [position.GetY() + offset_.GetY()] = value;
+  }
   public Renderer(int size) {
     size_ = size;
     screen_ = new char[size][size];
+    offset_ = new Vector2D(0, 0);
   };
   public void Fill(char value) {
     for (int i = 0; i < screen_.length; ++i) {
@@ -15,6 +21,54 @@ public class Renderer {
         screen_[i][j] = value; 
       }
     }
+  }
+  public void RenderSurface(Vector2D point_1, Vector2D point_2,
+                            Vector2D point_3) {
+    int min_x = Math.min(point_1.GetX(),
+                         Math.min(point_2.GetX(), point_3.GetX()));
+    int min_y = Math.min(point_1.GetY(),
+                         Math.min(point_2.GetY(), point_3.GetY()));
+    int max_x = Math.max(point_1.GetX(),
+                         Math.max(point_2.GetX(), point_3.GetX()));
+    int max_y = Math.max(point_1.GetY(),
+                         Math.max(point_2.GetY(), point_3.GetY()));
+    char real_screen[][] = screen_;
+    Vector2D real_offset = offset_;
+    screen_ = new char[max_x - min_x + 1][max_y - min_y + 1];
+    offset_ = new Vector2D(-min_x, -min_y);
+    Fill(' ');
+    RenderLine(point_1, point_2);
+    RenderLine(point_1, point_3);
+    RenderLine(point_2, point_3);
+    for (int i = 0; i < screen_.length; ++i) {
+      int up = 0, down = 0;
+      for (int j = 0; j < screen_[0].length; ++j) {
+        if (screen_[i][j] != ' ') {
+          down = j;
+          break;
+        }
+      }
+      for (int j = screen_[0].length - 1; j >= 0; --j) {
+        if (screen_[i][j] != ' ') {
+          up = j;
+          break;
+        }
+      }
+      while (down <= up) {
+        screen_[i][down] = '#';
+        ++down;
+      }
+    }
+    char surface[][] = screen_;
+    screen_ = real_screen;
+    for (int i = 0; i < surface.length; ++i) {
+      for (int j = 0; j < surface[0].length; ++j) {
+        if (surface[i][j] != ' ') {
+          SetScreen(new Vector2D(i, j), surface[i][j]);
+        }
+      }
+    }
+    offset_ = real_offset;
   }
   public void RenderLine(Vector2D point_1, Vector2D point_2) {
     int delta_x = point_2.GetX() - point_1.GetX();
@@ -26,9 +80,6 @@ public class Renderer {
       delta_x = -delta_x;
       delta_y = -delta_y;
     }
-    point_1.Print();
-    point_2.Print();
-    System.out.print(delta_x + " " + delta_y + " " + delta_delta_x_y);
     int start, end;
     if (delta_delta_x_y > 0) {
       start = point_1.GetX();
@@ -40,16 +91,16 @@ public class Renderer {
     for (int now = 0; start + now - 1 != end; ++now) {
       int value;
       if (delta_delta_x_y > 0) {
-        value = now * delta_y / delta_x + point_1.GetY();
-        screen_[start + now][Math.round(value)] = '1';
+        value = Math.round((float)(now * delta_y) / delta_x) + point_1.GetY();
+        SetScreen(new Vector2D(start + now, value), '1');
       }else {
-        value = now * delta_x / delta_y + point_1.GetX();
-        screen_[Math.round(value)][start + now] = '1';
+        value = Math.round((float)(now * delta_x) / delta_y) + point_1.GetX();
+        SetScreen(new Vector2D(value, start + now), '1');
       }
     }
   }
   public void RenderPoint(Vector2D point) {
-    screen_[point.GetX()][point.GetY()] = 'O';
+    SetScreen(point, 'O');
   }
   public void Render(Space space) {
     for (Vector3D point: space.points_) {
